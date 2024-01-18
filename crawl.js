58b7ce6684dbd95f7a763234892999b0f59b9be6
marc <mcadoo300@gmail.com>
2024-01-18 14:22:55 -0500

    begin with processing html from URL, using JSDOM

--- crawl.js
+++ crawl.js
@@ -1,14 +1,27 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+
 function normalizeURL(URL) {
   const host = URL.host
   const port = URL.port
   const path = URL.pathname
   const url_comp = [host,port,path]
-  console.log(URL);
   return url_comp.join('')
 }
 
-normalizeURL(new URL("https://www.demo-site.org/products/item?id=12345"));
+function getURLFromHTML(htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody);
+  console.log(dom.window.document.querySelector("a").href);
+}
 
+const html_text = '<html>' +
+    '<body>' + 
+        '<a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>' +
+    '</body>' +
+'</html>';
+
+getURLFromHTML(html_text, new URL("https://www.testme.com"));
 
 module.exports = {
   normalizeURL
