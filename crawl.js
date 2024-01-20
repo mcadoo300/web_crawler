3da4321e8f56b106d551efa0b5608531f89a37e7
marc <mcadoo300@gmail.com>
2024-01-20 14:16:35 -0500

    getURLfromHTML body function implementation

--- crawl.js
+++ crawl.js
@@ -3,25 +3,50 @@ const { JSDOM } = jsdom;
 
 
 function normalizeURL(URL) {
-  const host = URL.host
-  const port = URL.port
-  const path = URL.pathname
-  const url_comp = [host,port,path]
-  return url_comp.join('')
+  try {
+    const host = URL.host
+    const port = URL.port
+    const path = URL.pathname
+    const url_comp = [host,port,path]
+    return url_comp.join('')
+
+  } catch (error) {
+    console.log(`error with normalizing url: ${error.message}`) 
+  }
 }
 
 function getURLFromHTML(htmlBody, baseURL) {
+  const linked_URLS = [];
   const dom = new JSDOM(htmlBody);
-  console.log(dom.window.document.querySelector("a").href);
+  const hyper_links = dom.window.document.querySelectorAll('a');
+  for (const linkElement of hyper_links){
+    if (linkElement.href.slice(0,1) === '/'){
+      //relative
+      try {
+        const newURL = new URL(`${baseURL.origin}${linkElement.href}`);
+        linked_URLS.push(newURL);
+      } catch (error) {
+        console.log(`error with relative url: ${error.message}`);
+      }
+    } else {
+      try {
+        const newURL = new URL(linkElement.href);
+        linked_URLS.push(newURL);
+      } catch (error) {
+       console.log(`error with absolute url: ${error.message}`); 
+      }
+    }
+  }
+  return linked_URLS;
 }
 
 const html_text = '<html>' +
     '<body>' + 
-        '<a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>' +
+        '<a href="/boot.dev/"><span>Go to Boot.dev</span></a>' +
     '</body>' +
 '</html>';
 
-getURLFromHTML(html_text, new URL("https://www.testme.com"));
+getURLFromHTML(html_text, new URL("https://testme.com"));
 
 module.exports = {
   normalizeURL
